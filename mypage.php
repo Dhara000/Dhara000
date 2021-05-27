74e2019a7a05349a844bec642a4c512d9199dffc
rukaiya <rukaiyadhara00@gmail.com>
2021-05-27 10:34:35 -0500

    first

---
+++ mypage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>test</title>
+	</head>
+	<body>
+		<h1>Home Page</h1>
+		<marquee><h3>*Free Palestine Movement*</h3></marquee>
+		<a href="https://github.com/rahulhassan">GitHub Profile</a>
+		<form>
+			<label for="fname">First name:</label><br>
+			<input type="text" id="fname" name="fname"><br>
+			<label for="lname">Last name:</label><br>
+			<input type="text" id="lname" name="lname"><br>
+
+			<input type="radio" id="male" name="gender" value="male">
+			<label for="male">Male</label><br>
+			<input type="radio" id="female" name="gender" value="female">
+			<label for="female">Female</label><br>
+			<input type="radio" id="other" name="gender" value="other">
+			<label for="other">Other</label><br>
+
+			Choose your vehicles: <br>
+			<input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+			<label for="vehicle1">Bike</label><br>
+			<input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+			<label for="vehicle2">Car</label><br>
+			<input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+			<label for="vehicle3">Boat</label><br>
+
+			<input type="submit">
+			<input type="reset">
+		</form>
+	</body>
+</html>
